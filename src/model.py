49a910380ea475d6c9daf80db0dfb22a627aecb7
kenya-sk <ksakka0309@gmail.com>
2019-04-19 01:08:38 +0900

    dev:

--- src/model.py
+++ src/model.py
@@ -14,12 +14,12 @@ class Encoder(nn.Module):
         self.hidden_size = hidden_size
         self.embed_size = embed_size
         self.embed = nn.Embedding(input_size, embed_size)
-        self.lstm_cell = nn.LSTMCell(embed_size, hidden_size, n_layers,
+        self.gru = nn.GRU(embed_size, hidden_size, n_layers,
                           dropout=dropout, bidirectional=True)
 
     def forward(self, src, hidden=None):
         embedded = self.embed(src)
-        outputs, hidden = self.lstm_cell(embedded, hidden)
+        outputs, hidden = self.gru(embedded, hidden)
         # sum bidirectional outputs
         outputs = (outputs[:, :, :self.hidden_size] +
                    outputs[:, :, self.hidden_size:])
@@ -62,7 +62,7 @@ class Decoder(nn.Module):
         self.embed = nn.Embedding(output_size, embed_size)
         self.dropout = nn.Dropout(dropout, inplace=True)
         self.attention = Attention(hidden_size)
-        self.lstm_cell = nn.LSTMCell(hidden_size + embed_size, hidden_size,
+        self.gru = nn.GRU(hidden_size + embed_size, hidden_size,
                           n_layers, dropout=dropout)
         self.out = nn.Linear(hidden_size * 2, output_size)
 
