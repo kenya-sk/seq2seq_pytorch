ca6e89dbb0575dfceef5fbcc072470c89abc7133
kenya-sk <ksakka0309@gmail.com>
2019-04-20 20:53:52 +0900

    dev:

--- src/train.py
+++ src/train.py
@@ -1,13 +1,9 @@
-import os
-import math
 import logging
 import argparse
 import pickle
 from tqdm import tqdm
 import torch
-import torch.nn as nn
 from torch import optim
-from torch.autograd import Variable
 from torch.nn.utils import clip_grad_norm
 from torch.nn import functional as F
 from model import Encoder, Decoder, Seq2Seq
@@ -25,8 +21,8 @@ def evaluate(model, val_iter, vocab_size, en_vocab, de_vocab):
         trg = trg.data.cuda()
         output = model(src, trg, teacher_forcing_ratio=0.0)
         loss = F.nll_loss(output[1:].view(-1, vocab_size),
-                               trg[1:].contiguous().view(-1),
-                               ignore_index=pad)
+                          trg[1:].contiguous().view(-1),
+                          ignore_index=pad)
         total_loss += loss.item()
     return total_loss / len(val_iter)
 
@@ -40,8 +36,8 @@ def train(epoch, model, optimizer, train_iter, vocab_size, grad_clip, en_vocab, 
         optimizer.zero_grad()
         output = model(src, trg)
         loss = F.nll_loss(output[1:].view(-1, vocab_size),
-                               trg[1:].contiguous().view(-1),
-                               ignore_index=pad)
+                          trg[1:].contiguous().view(-1),
+                          ignore_index=pad)
         loss.backward()
         clip_grad_norm(model.parameters(), grad_clip)
         optimizer.step()
@@ -75,13 +71,13 @@ def main(args):
     # setting seq2seq model 
     logger.debug("Instantiating models...")
     encoder = Encoder(en_size, args.embed_dim, args.hidden_dim,
-                    n_layers=args.en_n_layers, dropout=args.en_dropout)
+                      n_layers=args.en_n_layers, dropout=args.en_dropout)
     decoder = Decoder(args.embed_dim, args.hidden_dim, de_size,
-                    n_layers=args.de_n_layers, dropout=args.de_dropout)
+                      n_layers=args.de_n_layers, dropout=args.de_dropout)
     seq2seq = Seq2Seq(encoder, decoder).cuda()
     if args.pre_trained_path is not None:
         seq2seq.load_state_dict(torch.load(args.pre_trained_path))
-        logger.debug("Load pretrained  model: {0}".format(args.pre_trained_path))
+        logger.debug("Load pre trained  model: {0}".format(args.pre_trained_path))
     optimizer = optim.Adam(seq2seq.parameters(), lr=args.lr)
     logger.debug(seq2seq)
 
@@ -89,15 +85,15 @@ def main(args):
     best_val_loss = None
     for epoch in range(1, args.epochs+1):
         train(epoch, seq2seq, optimizer, train_iter,
-            de_size, args.grad_clip, en_vocab, de_vocab)
+              de_size, args.grad_clip, en_vocab, de_vocab)
         val_loss = evaluate(seq2seq, val_iter, de_size, en_vocab, de_vocab)
         logger.debug("VAL LOSS: {0:.5f} (epoch={1})".format(val_loss, epoch))
 
-    # Save the model if the validation loss is the best we've seen so far.
-    if (best_val_loss is None) or (val_loss < best_val_loss):
-        logger.debug("save model (epoch={0})".format(epoch))
-        torch.save(seq2seq.state_dict(), args.save_model_path)
-        best_val_loss = val_loss
+        # Save the model if the validation loss is the best we've seen so far.
+        if (best_val_loss is None) or (val_loss < best_val_loss):
+            logger.debug("save model (epoch={0})".format(epoch))
+            torch.save(seq2seq.state_dict(), args.save_model_path)
+            best_val_loss = val_loss
 
 
 def make_parse():
@@ -110,12 +106,12 @@ def make_parse():
     )
 
     # data Argument
-    parser.add_argument("--train_path", type=str, default="/content/gdrive/My Drive/seq2seq_pytorch/data/datasets/train.csv")
-    parser.add_argument("--val_path", type=str, default="/content/gdrive/My Drive/seq2seq_pytorch/data/datasets/val.csv")
-    parser.add_argument("--en_vocab_path", type=str, default="/content/gdrive/My Drive/seq2seq_pytorch/data/vocab/en_vocab.pth")
-    parser.add_argument("--de_vocab_path", type=str, default="/content/gdrive/My Drive/seq2seq_pytorch/data/vocab/de_vocab.pth")
+    parser.add_argument("--train_path", type=str, default="../data/datasets/train.csv")
+    parser.add_argument("--val_path", type=str, default="../data/datasets/val.csv")
+    parser.add_argument("--en_vocab_path", type=str, default="../data/vocab/en_vocab.pth")
+    parser.add_argument("--de_vocab_path", type=str, default="../data/vocab/de_vocab.pth")
     parser.add_argument("--pre_trained_path", type=str, default=None)
-    parser.add_argument("--save_model_path", type=str, default="/content/gdrive/My Drive/seq2seq_pytorch/data/model/best_model.pth")
+    parser.add_argument("--save_model_path", type=str, default="../data/model/best_model.pth")
 
     # params Argument
     parser.add_argument("--en_n_layers", type=int, default=2)
@@ -138,7 +134,7 @@ def make_parse():
 
 
 if __name__ == "__main__":
-    logs_path = "/content/gdrive/My Drive/seq2seq_pytorch/logs/train.log"
+    logs_path = "../logs/train.log"
     logging.basicConfig(filename=logs_path,
                         level=logging.DEBUG,
                         format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
